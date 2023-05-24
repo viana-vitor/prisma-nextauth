3a2bec1be7644c9588f6d3f98ff3bb061f401116
Vitor Viana <vitorgovian@gmail.com>
2023-05-23 19:53:42 -0700

    added callbacks, jwt strategy, types. Done testing

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -7,6 +7,9 @@ import prisma from "@/lib/prisma";
 
 export const authOptions: AuthOptions = {
     adapter: PrismaAdapter(prisma),
+    session: {
+        strategy: "jwt"
+    },
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID!,
@@ -16,6 +19,38 @@ export const authOptions: AuthOptions = {
     secret: process.env.NEXTAUTH_SECRET,
     pages: {
         signIn: "/signin"
+    },
+    callbacks: {
+        async jwt({token, user}) {
+            const dbUser = await prisma.user.findFirst({
+                where: {
+                    email: token.email,
+                },
+            })
+            
+            if (!dbUser) {
+                if (user) {
+                    token.id = user.id
+                }
+                return token
+            }
+            
+            return {
+                id: dbUser.id,
+                name: dbUser.name,
+                email: dbUser.email,
+                picture: dbUser.image,
+            }
+        },
+        async session({token, session}) {
+            if (token) {
+                session.user.id = token.id
+                session.user.name = token.name
+                session.user.email = token.email
+                session.user.image = token.picture
+            }
+            return session
+        },
     }
 
 }
--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,14 +1,25 @@
 "use client"
-import { signIn } from "next-auth/react"
-import { useSearchParams } from "next/navigation"
+import { signIn, useSession } from "next-auth/react"
+import { redirect, useSearchParams } from "next/navigation"
 
 export default function SignIn() {
+  const { data: session } = useSession()
   const searchParams = useSearchParams()
   const callbackUrl = searchParams.get("callbackurl")
 
+  if (session) {
+    redirect("/")
+  }
+
   return (
     <div>
-      <button onClick={() => signIn("google", { callbackUrl: callbackUrl })}>
+      <button
+        onClick={() =>
+          signIn("google", {
+            callbackUrl: callbackUrl ? callbackUrl : "/",
+          })
+        }
+      >
         Signin
       </button>
     </div>
